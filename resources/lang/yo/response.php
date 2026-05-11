b07c027a67d835358262ef54c9b0d4a96e13ac70
Abraham Jesulayomi Olaobaju <39011309+bajoski34@users.noreply.github.com>
2026-05-11 02:04:43 +0000

    add yoruba translation files

---
+++ resources/lang/yo/response.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'errors' => [
+        'server_error' => 'Àṣìṣe inú sẹ́ńtà',
+        'record_not_found' => 'A kò rí :attribute kankan!',
+    ],
+
+    'attributes' => [
+        'phone' => 'nọ́ńbà fóònù|àwọn nọ́ńbà fóònù',
+        'country' => 'orílẹ̀-èdè|àwọn orílẹ̀-èdè',
+        'city' => 'ìlú|àwọn ìlú',
+        'state' => 'ìpínlẹ̀|àwọn ìpínlẹ̀',
+        'timezone' => 'agbègbè àkókò|àwọn agbègbè àkókò',
+        'currency' => 'owó orílẹ̀-èdè|àwọn owó orílẹ̀-èdè',
+        'language' => 'èdè|àwọn èdè',
+        'geolocate' => 'ìtọ́kasí ibi|àwọn ìtọ́kasí ibi',
+    ],
+];
